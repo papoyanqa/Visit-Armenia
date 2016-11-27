f50a1feaf4a3fddaf28706e6163481e8cf3ec477
Gagik Papoyan <papoyan.qa@gmail.com>
2016-11-28 00:55:02 +0400

    fix

--- Visit Armenia/ViewDetailsCell.swift
+++ Visit Armenia/ViewDetailsCell.swift
@@ -12,4 +12,16 @@ class ViewDetailsCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 8
+        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        self.imageView.layer.cornerRadius = 5.0
+        self.imageView.layer.borderWidth = 0.5
+        self.imageView.layer.masksToBounds = true
+        self.imageView.layer.borderColor = UIColor.clear.cgColor
+    }
 }
