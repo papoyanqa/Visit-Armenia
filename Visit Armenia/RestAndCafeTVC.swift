bd402de8f145565d12cf78cb46b5231349b07eec
Gagik Papoyan <papoyan.qa@gmail.com>
2016-11-29 17:56:31 +0400

    bagrat demotivation

---
+++ Visit Armenia/RestAndCafeTVC.swift
@@ -0,0 +1,24 @@
+//
+//  RestAndCafeTVC.swift
+//  Visit Armenia
+//
+//  Created by Gagik Papoyan on 11/29/16.
+//  Copyright © 2016 Gagik Papoyan. All rights reserved.
+//
+
+import UIKit
+
+class RestAndCafeTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
